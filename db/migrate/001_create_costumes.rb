a91b49ccf7b5d256746efaa327d1042a685ad486
Leah Hirschfeld <leah.hirschfeld@gmail.com>
2020-11-15 21:01:37 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -12,6 +12,9 @@ class CreateCostumes < ActiveRecord::Migration[5.1]
   def change
     create_table :costumes do |t|
       t.string :name
+      t.integer :price
+      t.size
+      t.image_url
     end
   end
 end
