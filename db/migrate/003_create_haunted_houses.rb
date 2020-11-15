09f55d71072610589e7d3ac2858f2e967a122bcc
Leah Hirschfeld <leah.hirschfeld@gmail.com>
2020-11-15 21:13:10 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -6,8 +6,8 @@ class CreateHauntedHouse < ActiveRecord::Migration[5.1]
       t.text :theme
       t.float :price
       t.boolean :family_friendly
-      t.datetime :opening_time
-      t.datetime :closing_time
+      t.datetime :opening_date
+      t.datetime :closing_date
     end
   end
 end
